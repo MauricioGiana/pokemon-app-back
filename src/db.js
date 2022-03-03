65f31ef8268089b535775b4dce6e0e3d9779f97d
Mauricio Giana <jmauricio@hotmail.com.ar>
2022-03-02 22:51:00 -0300

    create responsive

--- src/db.js
+++ src/db.js
@@ -12,12 +12,12 @@ const {
 }); */
 
 const sequelize = new Sequelize(DATABASE_URL, {
-  dialectOptions: {
+  /* dialectOptions: {
     ssl: {
       require: true,
       rejectUnauthorized: false
     }
-  },
+  }, */
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
